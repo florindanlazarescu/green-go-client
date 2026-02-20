ba44045eb56f1461a69d8c781056904cdfd98bef
CiprianBunescu <ciprian.bunescu@gmail.com>
2026-02-20 21:29:01 +0200

    builty gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             buildConfigField("String", "BASE_URL", "\"https://www.green-go.ro/\"")
         }
         create("staging") {
